230e25d240de82e90c41d4861bc80a65f050774b
alexey <pereligins@gmail.com>
2019-11-21 16:02:51 +0300

    Redux setup

---
+++ screens/shop/CartScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, FlatList} from "react-native";
+
+const CartScreen = props => {
+    return (<View></View>);
+}
+
+styles = StyleSheet.create({
+
+});
+
+export default CartScreen;
---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, FlatList} from "react-native";
+
+const ProductsOverviewScreen = props => {
+    return (<FlatList />);
+}
+
+styles = StyleSheet.create({
+
+});
+
+export default ProductsOverviewScreen;
