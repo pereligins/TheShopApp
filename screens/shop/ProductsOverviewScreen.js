1f2ccbfb2f0e925aa16ecdbd8d0507f9faaa966e
alexey <pereligins@gmail.com>
2019-11-22 01:35:12 +0300

    Navigation setup part 1

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import {View, Text, FlatList} from "react-native";
+import {useSelector} from "react-redux";
 
 const ProductsOverviewScreen = props => {
-    return (<FlatList />);
+
+    const products = useSelector(state => state.products.availableProducts);
+
+    return (<FlatList data={products} keyExtractor={item => item.id} renderItem={itemData => <Text>itemData.title</Text>}/>);
 }
 
 styles = StyleSheet.create({
