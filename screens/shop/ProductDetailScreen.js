d7fd4daf5d69f1623971e977212d844b920a55c8
alexey <pereligins@gmail.com>
2019-12-10 01:59:30 +0300

    Adding items to the cart

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -1,12 +1,24 @@
 import React from "react";
-import {View, StyleSheet, Text} from "react-native";
-import {useSelector} from "react-redux";
+import {View, StyleSheet, Text, ScrollView, Image, Button} from "react-native";
+import {useSelector, useDispatch} from "react-redux";
+import Colors from "../../constants/Colors";
+import * as cartActions from "../../store/actions/cart"
 
 const ProductDetailScreen = props => {
     const productId = props.navigation.getParam('productId');
     const selectedProduct = useSelector(state => state.products.availableProducts.find(prod => prod.id === productId));
+    const dispatch = useDispatch();
 
-    return <View><Text>{selectedProduct.title}</Text></View>;
+    return <ScrollView>
+        <Image style={styles.image} source={{uri: selectedProduct.imageUrl}} />
+        <View style={styles.actions}>
+            <Button color={Colors.primary} title="Add to Card" onPress={() => {
+                dispatch(cartActions.addToCart(selectedProduct));
+            }}/>
+        </View>
+        <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+        <Text style={styles.description}>{selectedProduct.description}</Text>
+    </ScrollView>;
 }
 
 ProductDetailScreen.navigationOptions = navData => {
@@ -16,7 +28,27 @@ ProductDetailScreen.navigationOptions = navData => {
 }
 
 const styles = StyleSheet.create({
-
+    image: {
+        width: '100%',
+        height: 300
+    },
+    price: {
+        fontSize: 20,
+        color: "#888",
+        textAlign: 'center',
+        marginVertical: 20,
+        fontFamily: 'open-sans-bold'
+    },
+    description: {
+        fontSize: 14,
+        textAlign: 'center',
+        marginHorizontal: 20,
+        fontFamily: 'open-sans'
+    },
+    actions: {
+        marginVertical: 10,
+        alignItems: 'center'
+    }
 });
 
 export default ProductDetailScreen;
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {View, Text, FlatList} from "react-native";
-import {useSelector} from "react-redux";
+import {useSelector, useDispatch} from "react-redux";
 import ProductItem from "../../components/shop/ProductItem";
+import * as cartActions from "../../store/actions/cart"
 
 const ProductsOverviewScreen = props => {
 
     const products = useSelector(state => state.products.availableProducts);
+    const dispatch = useDispatch();
 
     return (<FlatList
         data={products}
@@ -22,6 +24,7 @@ const ProductsOverviewScreen = props => {
                                         });
                                 }}
                                 onAddToCard={() => {
+                                    dispatch(cartActions.addToCart(itemData.item))
                                 }}
             />
 
