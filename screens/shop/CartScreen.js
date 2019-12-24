42ffecce77cf3e235d466edcbf6bb455be69a6dd
Aleksey Perelygin <pereligins@gmail.com>
2019-12-24 22:48:31 +0300

    Delete Buttons

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,8 +1,9 @@
 import React from "react";
 import {View, Text, FlatList, StyleSheet, Button} from "react-native";
-import {useSelector} from "react-redux";
+import {useSelector, useDispatch} from "react-redux";
 import Colors from "../../constants/Colors";
 import CartItem from "../../components/shop/CartItem";
+import * as cartActions from '../../store/actions/cart'
 
 const CartScreen = props => {
     const cartTotalAmount = useSelector(state => state.cart.totalAmount);
@@ -17,8 +18,10 @@ const CartScreen = props => {
                 sum: state.cart.items[key].sum
             });
         }
-        return CartItemsArray;
+        return CartItemsArray.sort((a,b)=> a.productId > b.productId ? 1 : -1);
     });
+
+    const dispatch = useDispatch();
 
     return (<View style={styles.screen}>
         <View style={styles.summary}>
@@ -31,7 +34,7 @@ const CartScreen = props => {
                       itemData => <CartItem quantity={itemData.item.quantity}
                       title={itemData.item.productTitle}
                       amount={itemData.item.sum}
-                      onRemove={()=>{}}/>
+                      onRemove={()=>{dispatch(cartActions.removeFromCart(itemData.item.productId))}}/>
                   }/>
     </View>);
 }
