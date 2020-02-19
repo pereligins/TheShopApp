0318c1e70ace91343ebd1a6fec70c2bb62ca100e
Aleksey Perelygin <pereligins@gmail.com>
2020-02-19 16:43:37 +0300

    deleting

--- store/actions/products.js
+++ store/actions/products.js
@@ -37,10 +37,18 @@ export const fetchProducts = () => {
 };
 
 export const deleteProduct = productId => {
-    return {
-        type: DELETE_PRODUCT,
-        pid: productId
-    };
+
+    return async dispatch => {
+
+        await fetch(`https://udemy-education-project.firebaseio.com/products/${productId}.json`, {
+            method: 'DELETE'
+        });
+
+        dispatch({
+            type: DELETE_PRODUCT,
+            pid: productId
+        });
+    }
 }
 
 export const createProduct = (title, description, imageUrl, price) => {
